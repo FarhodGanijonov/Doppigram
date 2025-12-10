fa0ae295fed65a3202ac124d80b9c630d0fbf4c5
Farhod Ganijonov <farhod089@gmail.com>
2025-12-10 16:39:30 +0500

    Test CI/CD workflow 5

--- messenger/urls.py
+++ messenger/urls.py
@@ -5,7 +5,7 @@ from .views import ChatListView, ChatCreateView, MessageListView
 urlpatterns = [
     path('chats/', ChatListView.as_view(), name='chat-list'),
     path('chatsa/create/', ChatCreateView.as_view(), name='chat-create'),
-    path('messages/', MessageListView.as_view(), name='message-list'),
+    path('messagess/', MessageListView.as_view(), name='message-list'),
     # path('messages/upload/', UploadMessageView.as_view(), name='message-upload'),
 
 ]
