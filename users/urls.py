f234759b5eb02c645e50fb8b784afb007863c72f
Farhod Ganijonov <farhod089@gmail.com>
2025-12-30 11:23:04 +0500

    add deploy test url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
+from django.http import HttpResponse
 from django.urls import path
 from users.views import UserPasswordChangeView, UserRegistrationView, UserLoginView, UserMeView, UserUpdateProfileView, \
     LogoutView, ContactSearchView, UserListView
 
+def deploy_test(request):
+    return HttpResponse("DEPLOY_OK")
+
 urlpatterns = [
+    path("DEPLOY_TEST/", deploy_test),
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('change-password/', UserPasswordChangeView.as_view(), name='user-change-password'),
